83090514f2e187edf4387fc7e597abd5e78c63d9
Baodt2911 <dobao2911bs@gmail.com>
2025-12-07 13:01:58 +0700

    Fix firebase

--- app.config.js
+++ app.config.js
@@ -1,67 +1,46 @@
-// app.config.js
-export default {
-  expo: {
-    name: "CB | Chat",
-    slug: "Chat-app",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/favicon.png",
-    userInterfaceStyle: "light",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
+module.exports = () => ({
+  name: "CB | Chat",
+  slug: "Chat-app",
+  version: "2.0.0",
+
+  extra: {
+    eas: {
+      projectId: "c99f33be-2e64-41ed-85d4-73a127bca8b0",
+    },
+  },
+
+  android: {
+    package: "com.baodt2911.Chatapp",
+    adaptiveIcon: {
+      foregroundImage: "./assets/favicon.png",
       backgroundColor: "#ffffff",
     },
-    assetBundlePatterns: ["**/*"],
-
-    ios: {
-      bundleIdentifier: "com.baodt2911.Chatapp",
-      supportsTablet: true,
-      googleServicesFile:
-        process.env.GOOGLE_SERVICE_INFO_PLIST ?? "./GoogleService-Info.plist",
-    },
-
-    android: {
-      package: "com.baodt2911.Chatapp",
-      adaptiveIcon: {
-        foregroundImage: "./assets/favicon.png",
-        backgroundColor: "#ffffff",
-      },
-      googleServicesFile:
-        process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
-    },
-
-    web: {
-      favicon: "./assets/favicon.png",
-    },
-
-    extra: {
-      eas: {
-        projectId: "c99f33be-2e64-41ed-85d4-73a127bca8b0",
-      },
-    },
-
-    plugins: [
-      "@react-native-google-signin/google-signin",
-
-      [
-        "expo-image-picker",
-        {
-          photosPermission:
-            "The app accesses your photos to let you share them with your friends.",
-        },
-      ],
-
-      [
-        "react-native-fbsdk-next",
-        {
-          appID: "1281020842772304",
-          clientToken: "65257bb46ef078e42610b7ed141b8687",
-          displayName: "Chat app",
-        },
-      ],
-
-      "expo-font",
-    ],
+    googleServicesFile: "./google-services.json", // Đường dẫn trực tiếp
   },
-};
+
+  ios: {
+    bundleIdentifier: "com.baodt2911.Chatapp",
+    supportsTablet: true,
+    googleServicesFile: "./GoogleService-Info.plist", // Đường dẫn trực tiếp
+  },
+
+  plugins: [
+    "@react-native-google-signin/google-signin",
+    [
+      "expo-image-picker",
+      {
+        photosPermission:
+          "The app accesses your photos to let you share them with your friends.",
+      },
+    ],
+    [
+      "react-native-fbsdk-next",
+      {
+        appID: "1281020842772304",
+        clientToken: "65257bb46ef078e42610b7ed141b8687",
+        displayName: "Chat app",
+      },
+    ],
+    "expo-font",
+  ],
+});
